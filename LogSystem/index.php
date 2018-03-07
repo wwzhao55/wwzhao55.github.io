8fe6b75155d8b4a39830020b0b102f7e05800520
zhaoweiwei <435951924@qq.com>
2018-03-07 08:48:16 +0800

    update input.html

--- LogSystem/index.php
+++ LogSystem/index.php
@@ -1,9 +1,48 @@
 <?php
-      $conn = @mysql_connect("localhost","root","")
-          or die("数据库连接失败，请检查你的网络,稍后再试试");
-     mysql_select_db("test");
-     mysql_query("set names 'utf8'");//为中文编码集
-     $sql = "INSERT INTO `test`.`message` (`id`,`name`, `password`)
-     VALUES (NULL,'赵小样', '123456')";   
-     mysql_query($sql);//结束连接数据库；
+    $conn = mysql_connect("localhost","root","");//connect mysql
+    if(!$conn){
+        die("数据库连接失败，请检查你的网络,稍后再试试");
+   	}
+	/* create database
+    if (mysql_query("CREATE DATABASE logsystem",$conn)){
+		echo "Database created";
+	}else{
+		echo "Error creating database: " . mysql_error();
+  	}
+  	*/
+  	/*create table in logsystem database
+  	
+  	mysql_select_db("logsystem",$conn);
+  	$sql = "CREATE TABLE User
+  	(
+  		name varchar(15),
+  		password varchar(15)
+  	)";
+  	mysql_query($sql,$conn);
+  	
+  	create table end
+  	*/
+  	/*insert data start
+
+    mysql_select_db("logsystem",$conn);
+    mysql_query( "INSERT INTO User (id, name, password)
+    VALUES (3,'Admin', '123456')");  
+
+     */
+    mysql_select_db("Logsystem", $con);
+
+	$result = mysql_query("SELECT * FROM User WHERE name='admin'");
+  // while($row = mysql_fetch_array($result)){
+  //     echo $row['name'] . " " . $row['password'];
+  // }
+	while($row = mysql_fetch_array($result)){
+	  echo json_decode(array(
+	  	'status' =>"success",
+	  	'message'=>'查询成功',
+	  	'name'=>$row['name'],
+	  	'password' =>$row['password']
+	  ));
+	  exit;
+	}
+    mysql_close($conn);//结束连接数据库；
  ?>
